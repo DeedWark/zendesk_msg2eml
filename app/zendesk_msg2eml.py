9352954ca03c868b292909dfc91956436d0b0e88
DeedWark <deedwark@hotmail.fr>
2021-07-15 13:32:13 +0200

    Add exception and check_ouput for subprocess

--- app/zendesk_msg2eml.py
+++ app/zendesk_msg2eml.py
@@ -3,7 +3,6 @@ import time
 import shutil
 import requests
 import subprocess
-import configparser
 from zenpy import Zenpy
 
 # CONFIG PARSER
@@ -16,7 +15,7 @@ view_id = int(view_id)
 author_id = config.get('ZENDESK', 'author_id')
 author_id = int(author_id)
 DIR = config.get('SYSTEM', 'directory')
-# Env var
+# ENV VAR
 token = os.environ.get("ZENDESK_TOKEN")
 
 creds = {
@@ -27,16 +26,17 @@ creds = {
 zenpy = Zenpy(**creds)
 
 def msg2eml(view_id):
-    """ CHECK DIR, IF NOT EXISTS, CREATE IT ELSE REMOVE AND RECREATE IT """
+    DIR = "msg"
     if not os.path.exists(DIR):
         os.mkdir(DIR)
     else:
         shutil.rmtree(DIR, ignore_errors=True)
         os.mkdir(DIR)
-        
+
     """ EXECUTE VIEW IN ORDER TO GET TICKETS """
     for ticket in zenpy.views.tickets(view=view_id):
         ticket_id = ticket.id
+        print(ticket_id)
 
         """ GET ALL COMMENT, ALL ATTACHMENTS AND THEIR CONTENT_URL, CONTENT_TYPE AND FILE_NAME """
         for comment in zenpy.tickets.comments(ticket_id):
@@ -55,10 +55,15 @@ def msg2eml(view_id):
         """ CONVERT ALL MSG FILE IN MSG DIR WITH RUBY MSG CONVERTER """
         for file in os.listdir(DIR):
             if file.lower().endswith(".msg"):
+                print(file)
                 try:
-                    subprocess.run(["mapitool", "-i", DIR+"/"+file, "-o", DIR])
-                except (RuntimeError, Exception) as err:
-                    print(err)
+                    subprocess.check_output(["mapitool", "-i", DIR+"/"+f"{file}", "-o", DIR],
+                        stderr=subprocess.STDOUT,
+                        shell=False
+                    )
+                except subprocess.CalledProcessError as err:
+                    print(f"Failed to convert {file} MSG to EML, please do it manually!", err)
+                    pass
             else:
                 pass
 
@@ -67,16 +72,27 @@ def msg2eml(view_id):
 
         for file in os.listdir(DIR):
             if file.lower().endswith(".eml"):
-                upload_instance = zenpy.attachments.upload(DIR+"/"+file)
-                ticket = zenpy.tickets(id=ticket_id)
-                ticket.comment = Comment(
-                    body=f"File: {file_name} converted from MSG to EML - New File: {file}",
-                    uploads=[upload_instance.token],
-                    public=False,
-                    author_id=author_id
+                file_path = DIR+"/"+file
+                print(file_path)
+                try:
+                    upload_instance = zenpy.attachments.upload(file_path)
+                    ticket = zenpy.tickets(id=ticket_id)
+                    ticket.comment = Comment(
+                        body=f"File {file[:-4]}.msg converted from MSG to EML - New File: {file}",
+                        uploads=[upload_instance.token],
+                        public=False,
+                        author_id=author_id
+                        )
+                    ticket.tags.extend(['msg2eml_done'])
+                except:
+                    ticket.comment = Comment(
+                        body=f"Failed to convert and upload {file[:-4]}.msg from MSG to EML, please try manually!",
+                        public=False,
+                        author_id=author_id
                     )
-                ticket.tags.extend(['msg2eml_done'])
-                zenpy.tickets.update(ticket)
+                    ticket.tags.extend(['msg2eml_failed'])
+                
+            zenpy.tickets.update(ticket)
 
         return True
 
@@ -87,3 +103,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+    
